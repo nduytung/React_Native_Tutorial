0d537e18a364ac974212c61bb37255f4ab7d01c8
nduytung <nduytung.1611@gmail.com>
2021-10-08 08:26:52 +0700

    xong bai 7

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View, Button, Linking} from 'react-native';
 
 //hàm này sẽ render ra tất cả các nội dung hiển thị
 const App = () => {
+  const [name, setName] = useState('Style test');
+
+  const onClickHandler = () => {
+    setName('Style test is done');
+  };
+
   return (
     //chèn file styles.body vào đây
     <View style={styles.body}>
-      <Text style={styles.text}>hello, my name is Mash </Text>
-      <Button
-        onPress={() =>
-          Linking.openURL('https://youtube.com/programmingwithmash')
-        }
-        title="Youtube channel"></Button>
+      <Text style={styles.text}> {name} </Text>
+      <View style={styles.button}>
+        <Button onPress={() => onClickHandler()} title="Update state"></Button>
+      </View>
     </View>
   );
 };
@@ -21,16 +25,25 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: 'blue',
     alignItems: 'center',
+    backgroundColor: 'yellow',
     justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: 'pink',
+    borderRadius: 10,
   },
 
   text: {
-    color: 'white',
+    color: 'black',
     fontSize: 30,
     fontStyle: 'italic',
     margin: 10,
+    textTransform: 'uppercase',
+  },
+
+  button: {
+    width: 200,
+    height: 60,
   },
 });
 
