2a4cb0ead742f124d6865acdcaeb6a56cab83cf8
nduytung <nduytung.1611@gmail.com>
2021-10-08 09:08:34 +0700

    done lesson 9

--- App.js
+++ App.js
@@ -1,27 +1,60 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, Button, Linking} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Linking,
+  ScrollView,
+  RefreshControl,
+} from 'react-native';
 
 //hàm này sẽ render ra tất cả các nội dung hiển thị
 const App = () => {
-  const [name, setName] = useState('Style test');
+  //danh sach cac item se duoc render
+  const [items, setItems] = useState([
+    {key: 1, item: 'item 1'},
+    {key: 2, item: 'item 2'},
+    {key: 3, item: 'item 3'},
+    {key: 4, item: 'item 4'},
+    {key: 5, item: 'item 5'},
+    {key: 6, item: 'item 6'},
+    {key: 7, item: 'item 7'},
+    {key: 8, item: 'item 8'},
+    {key: 9, item: 'item 8'},
+    {key: 10, item: 'item 9'},
+    {key: 11, item: 'item 10'},
+    {key: 12, item: 'item 11'},
+    {key: 13, item: 'item 12'},
+  ]);
 
-  const onClickHandler = () => {
-    setName('Style test is done');
+  //state này quản lý trạng thái refresh hay không
+  const [refresh, setRefresh] = useState(false);
+
+  const onRefresh = () => {
+    //nếu refresh thì set state để hiển thị nút xoay, sau đó thêm item vào và dừng nút xoay lại
+    setRefresh(true);
+
+    //thêm vào list cũ đã có bằng setState
+    setItems([...items, {key: 14, item: 'item 14'}]);
+    setRefresh(false);
   };
 
   return (
     //chèn file styles.body vào đây
-    <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.text}> 1</Text>
-      </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}> 2 </Text>
-      </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}> 3 </Text>
-      </View>
-    </View>
+    <ScrollView
+      //được gọi mỗi khi ta kéo xuống để refresh trang
+      refreshControl={
+        <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+      }>
+      {items.map(object => {
+        return (
+          <View style={styles.item} key={object.key}>
+            <Text style={styles.text}> {object.item} </Text>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
 };
 
@@ -37,31 +70,18 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
   },
 
-  view1: {
-    flex: 1,
+  item: {
     backgroundColor: 'cyan',
     justifyContent: 'center',
     alignItems: 'center',
+    margin: 10,
   },
 
-  view2: {
-    flex: 1,
-    backgroundColor: 'pink',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  view3: {
-    flex: 1,
-    backgroundColor: 'yellow',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   text: {
     color: 'black',
-    fontSize: 40,
+    fontSize: 30,
     fontStyle: 'italic',
     margin: 10,
-    textTransform: 'uppercase',
   },
 });
 
