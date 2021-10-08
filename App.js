e31d10d0f07cf60455c83a2900ab76708d8cc9f0
nduytung <nduytung.1611@gmail.com>
2021-10-08 09:43:49 +0700

    done lesson 10

--- App.js
+++ App.js
@@ -7,26 +7,51 @@ import {
   Linking,
   ScrollView,
   RefreshControl,
+  FlatList,
+  SectionList,
 } from 'react-native';
 
 //hàm này sẽ render ra tất cả các nội dung hiển thị
 const App = () => {
   //danh sach cac item se duoc render
   const [items, setItems] = useState([
-    {key: 1, item: 'item 1'},
-    {key: 2, item: 'item 2'},
-    {key: 3, item: 'item 3'},
-    {key: 4, item: 'item 4'},
-    {key: 5, item: 'item 5'},
-    {key: 6, item: 'item 6'},
-    {key: 7, item: 'item 7'},
-    {key: 8, item: 'item 8'},
-    {key: 9, item: 'item 8'},
-    {key: 10, item: 'item 9'},
-    {key: 11, item: 'item 10'},
-    {key: 12, item: 'item 11'},
-    {key: 13, item: 'item 12'},
+    {name: 'item 1'},
+    {name: 'item 2'},
+    {name: 'item 3'},
+    {name: 'item 4'},
+    {name: 'item 5'},
+    {name: 'item 6'},
+    {name: 'item 7'},
+    {name: 'item 8'},
+    {name: 'item 8'},
+    {name: 'item 9'},
+    {name: 'item 10'},
+    {name: 'item 11'},
+    {name: 'item 12'},
   ]);
+
+  const DATA = [
+    {
+      title: 'title 1',
+      data: ['Item 1-1', 'Item 1-2', 'Item 1-3'],
+    },
+    {
+      title: 'title 2',
+      data: ['Item 2-1', 'Item 2-2', 'Item 2-3'],
+    },
+    {
+      title: 'title 3',
+      data: ['Item 3-1', 'Item 3-2', 'Item 3-3'],
+    },
+    {
+      title: 'title 4',
+      data: ['Item 4-1', 'Item 4-2', 'Item 4-3'],
+    },
+    {
+      title: 'title 5',
+      data: ['Item 5-1', 'Item 5-2', 'Item 5-3'],
+    },
+  ];
 
   //state này quản lý trạng thái refresh hay không
   const [refresh, setRefresh] = useState(false);
@@ -36,25 +61,42 @@ const App = () => {
     setRefresh(true);
 
     //thêm vào list cũ đã có bằng setState
-    setItems([...items, {key: 14, item: 'item 14'}]);
+    setItems([...items, {name: 'item 14'}]);
     setRefresh(false);
   };
 
   return (
-    //chèn file styles.body vào đây
-    <ScrollView
-      //được gọi mỗi khi ta kéo xuống để refresh trang
+    //phan nay danh cho SectionList
+    <SectionList
       refreshControl={
         <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
-      }>
-      {items.map(object => {
-        return (
-          <View style={styles.item} key={object.key}>
-            <Text style={styles.text}> {object.item} </Text>
-          </View>
-        );
-      })}
-    </ScrollView>
+      }
+      keyExtractor={(item, index) => index.toString()}
+      numColumns={1}
+      sections={DATA}
+      // Tự động render ra những thành phần trong mảng con của DATA
+      renderItem={({item}) => <Text style={styles.text}> {item} </Text>}
+      renderSectionHeader={({section}) => (
+        <View style={styles.item}>
+          <Text style={styles.text}> {section.title} </Text>
+        </View>
+      )}
+    />
+
+    //phan nay danh cho FlatList
+    // <FlatList
+    //   refreshControl={
+    //     <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+    //   }
+    //   keyExtractor={(item, index) => index.toString()}
+    //   numColumns={1}
+    //   data={items}
+    //   renderItem={({item}) => (
+    //     <View style={styles.item}>
+    //       <Text style={styles.text}> {item.name} </Text>
+    //     </View>
+    //   )}
+    // />
   );
 };
 
