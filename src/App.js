74a67f7f3a3c60b65195f267a70b322a48b7d048
nduytung <nduytung.1611@gmail.com>
2021-10-14 08:08:19 +0700

    done lesson 17

--- src/App.js
+++ src/App.js
@@ -1,165 +1,79 @@
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Pressable,
-  Modal,
-  Image,
-  ImageBackground,
-} from 'react-native';
-import CustomButton from './CustomButton';
-import Header from './Header';
-//hàm này sẽ render ra tất cả các nội dung hiển thị
-const App = () => {
-  const [name, setName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
-  const [showWarning, setShowWarning] = useState(false);
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 
-  //kiểm tra điều kiện và render
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+const ScreenA = ({navigation}) => {
   const onPressHandler = () => {
-    if (name.length > 3) setSubmitted(!submitted);
-    else {
-      setShowWarning(true);
-    }
+    navigation.navigate('ScreenB');
+    //neu dung replace, screen B se tro thanh screen duy nhat ton tai, khong back duoc
+    // navigation.replace('ScreenB');
   };
 
   return (
-    <ImageBackground
-      style={styles.body}
-      //cách thứ nhất để chèn ảnh vào RN: URI
-      source={{
-        uri: 'https://apsachieveonline.org/wp-content/uploads/2020/02/1580739685_937_34-Hinh-nen-iOS-co-dien-danh-cho-iPhone-Ban.jpg',
-      }}>
-      <Header />
-      <Modal
-        visible={showWarning}
-        transparent
-        animationType={'fade'}
-        onRequestClose={() => setShowWarning(false)}>
-        <View style={styles.centered_view}>
-          <View style={styles.warning_modal}>
-            <View style={styles.warning_title}>
-              <Text style={styles.text}> WARNING! </Text>
-            </View>
-            <View style={styles.warning_body}>
-              <Text style={styles.text}>
-                The name must be longer than 3 characters
-              </Text>
-            </View>
-            <Pressable
-              style={styles.warning_button}
-              android_ripple={{color: 'white'}}
-              onPress={() => setShowWarning(false)}>
-              <Text style={styles.text}>OK</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Text style={styles.text}> Please write your name: </Text>
-      <TextInput
-        style={styles.input}
-        // multiline
-        // maxLength={4}
-        // keyboardType="decimal-pad"
-        placeholder="e.g. John"
-        onChangeText={value => setName(value)}
-      />
-
-      <CustomButton
-        onPressHandler={onPressHandler}
-        title={submitted ? 'Clear' : 'Submit'}
-        style={{margin: 40}}
-      />
-      {submitted ? (
-        <View style={styles.body}>
-          <Text style={styles.text}> You are registered as {name} </Text>
-          <Image
-            source={require('../assets/done.png')}
-            style={styles.image}
-            resizeMode="stretch"
-          />
-        </View>
-      ) : (
-        <Image
-          source={require('../assets/error.jpg')}
-          style={styles.image}
-          resizeMode="stretch"
-        />
-      )}
-    </ImageBackground>
+    <View style={styles.body}>
+      <Text style={styles.text}> Screen A</Text>
+      <Pressable
+        onPress={() => onPressHandler()}
+        style={({pressed}) => ({
+          backgroundColor: pressed ? 'white' : 'lime',
+        })}>
+        <Text style={styles.text}>Go to screen B</Text>
+      </Pressable>
+    </View>
   );
 };
 
-//ta có thể hiểu "styles" này gần giống như một file css, nó format lại định dạng hiển thị cho chúng ta
-//các classname gần tương tự như ở CSS, ta có thể sử dụng extension auto import để nó gợi ý
+const ScreenB = ({navigation}) => {
+  const onPressHandler = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}> Screen B</Text>
+      <Pressable
+        onPress={() => onPressHandler()}
+        style={({pressed}) => ({
+          backgroundColor: pressed ? 'white' : 'lime',
+        })}>
+        <Text style={styles.text}>Back to screen A</Text>
+      </Pressable>
+    </View>
+  );
+};
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        options={{
+          header: () => null,
+        }}>
+        {/* vì một số hạn chế của RN, đừng copy screen A rồi đổi tên thành screen B. HÃY GÕ MỚI HOÀN TOÀN  */}
+        <Stack.Screen
+          name="ScreenA"
+          component={ScreenA}
+          //tat header di bang option
+        />
+        <Stack.Screen name="ScreenB" component={ScreenB} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    alignItems: 'center',
-  },
-
-  item: {
-    backgroundColor: 'cyan',
     justifyContent: 'center',
     alignItems: 'center',
-    margin: 10,
   },
 
   text: {
-    color: 'black',
-    fontSize: 20,
-    textAlign: 'center',
-    alignItems: 'center',
-    margin: 10,
-  },
-
-  input: {
-    borderRadius: 5,
-    borderColor: 'white',
-    width: 200,
-    fontSize: 15,
-    borderWidth: 1,
-    textAlign: 'center',
-    marginBottom: 10,
-  },
-
-  centered_view: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    backgroundColor: '#00000099',
-  },
-  warning_modal: {
-    width: 300,
-    height: 300,
-    backgroundColor: 'white',
-    border: 1,
-    borderColor: 'black',
-    borderRadius: 20,
-    overflow: 'hidden',
-    //overflow:hidden để ta không cần set border cho 2 component header và nút ok kia như video
-  },
-  warning_title: {
-    backgroundColor: 'yellow',
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  warning_body: {
-    height: 200,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  warning_button: {
-    backgroundColor: 'cyan',
-  },
-
-  image: {
-    width: 100,
-    height: 100,
+    fontSize: 40,
+    fontWeight: 'bold',
     margin: 10,
   },
 });
