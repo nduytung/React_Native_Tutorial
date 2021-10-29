65bf33379cdda035fcb061c1934e82312bf5795b
nduytung <nduytung.1611@gmail.com>
2021-10-29 14:29:53 +0700

    done lesson 7

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('Style Test');
+  //bien name dung de lam vi du cho viec thay doi style trong component
+  const [name, setName] = useState('Test your style here !');
 
   const onClickHandler = () => {
     setName('Style Test is Done!');
@@ -12,24 +13,26 @@ const App = () => {
     <View style={styles.body}>
       <Text style={styles.text}>{name}</Text>
       <View style={styles.button}>
+        {/* moi button trong RN deu phai co title. Day chinh la ten cu button trong RN */}
         <Button title="Update State" onPress={onClickHandler}></Button>
       </View>
     </View>
   );
 };
 
+//Ve ban chat, stylesheet gan giong nhu mot file CSS, no se tao style cho tat ca component ma chung ta muon
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#ffff00',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 10,
-    borderColor: '#ff00ff',
+    borderColor: 'pink',
     borderRadius: 10,
   },
   text: {
-    color: '#000000',
+    color: 'black',
     fontSize: 35,
     fontStyle: 'italic',
     margin: 10,
